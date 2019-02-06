701accf4d40811cf77d3f515847686963134a221
3vikram <viku.vikram05@gmail.com>
2019-02-06 08:24:51 +0530

    Update SchoolAPI.py

--- SchoolAPI.py
+++ SchoolAPI.py
@@ -18,16 +18,23 @@ def get_student_data(studentid):
 
 @app.route('/student/create', methods=['POST'])
 def create_student():
+    headers = request.headers
+    auth = headers.get("API-Secret")
     data = request.get_json()
     student_name = data['sname']
     student_age = data['sage']
     student_class = data['sclass']
     student_section = data['ssection']
     create_student_info = RetrieveStudentInfo()
-    return jsonify(create_student_info.set_student_info(student_name, student_age, student_class, student_section))
+    if auth == "a1s2d3f4":
+        return jsonify(create_student_info.set_student_info(student_name, student_age, student_class, student_section))
+    else:
+        return jsonify({"message": "Unauthorised access"}), 403
 
 @app.route('/teacher/create', methods=['POST'])
 def create_teacher():
+    headers = request.headers
+    auth = headers.get("API-Secret")
     data = request.get_json()
     teacher_name = data['tname']
     teacher_age = data['tage']
@@ -35,7 +42,10 @@ def create_teacher():
     teacher_experience = data['texperience']
     teacher_salary = data['tsalary']
     create_teacher_info = RetrieveTeacherInfo()
-    return jsonify(create_teacher_info.set_teacher_info(teacher_name, teacher_age, teacher_qualification, teacher_experience, teacher_salary))
+    if auth == "a1s2d3f4":
+        return jsonify(create_teacher_info.set_teacher_info(teacher_name, teacher_age, teacher_qualification, teacher_experience, teacher_salary))
+    else:
+        return jsonify({"message": "Unauthorised access"}), 403
 
 @app.route('/teacher/count', methods=['GET'])
 def total_teachers():
@@ -49,20 +59,32 @@ def total_students():
 
 @app.route('/teacher/remove', methods=['DELETE'])
 def delete_teacher():
+    headers = request.headers
+    auth = headers.get("API-Secret")
     data = request.get_json()
     teacher_ID = data['tid']
     create_teacher_remove = RetrieveTeacherInfo()
-    return jsonify(create_teacher_remove.remove_teacher(teacher_ID))
+    if auth == "a1s2d3f4":
+        return jsonify(create_teacher_remove.remove_teacher(teacher_ID))
+    else:
+        return jsonify({"message": "Unauthorised access"}), 403
 
 @app.route('/student/remove', methods=['DELETE'])
 def delete_student():
+    headers = request.headers
+    auth = headers.get("API-Secret")
     data = request.get_json()
     student_ID = data['sid']
     create_student_remove = RetrieveStudentInfo()
-    return jsonify(create_student_remove.remove_student(student_ID))
+    if auth == "a1s2d3f4":
+        return jsonify(create_student_remove.remove_student(student_ID))
+    else:
+        return jsonify({"message": "Unauthorised access"}), 403
 
 @app.route('/teacher/update', methods=['PUT'])
 def update_teacher():
+    headers = request.headers
+    auth = headers.get("API-Secret")
     data = request.get_json()
     teacher_name = data['tname']
     teacher_age = data['tage']
@@ -70,17 +92,25 @@ def update_teacher():
     teacher_experience = data['texperience']
     teacher_salary = data['tsalary']
     create_teacher_info = RetrieveTeacherInfo()
-    return jsonify(create_teacher_info.update_teacher_info(teacher_name, teacher_age, teacher_qualification, teacher_experience, teacher_salary))
+    if auth == "a1s2d3f4":
+        return jsonify(create_teacher_info.update_teacher_info(teacher_name, teacher_age, teacher_qualification, teacher_experience, teacher_salary))
+    else:
+        return jsonify({"message": "Unauthorised access"}), 403
 
 @app.route('/student/update', methods=['PUT'])
 def update_student():
+    headers = request.headers
+    auth = headers.get("API-Secret")
     data = request.get_json()
     student_name = data['sname']
     student_age = data['sage']
     student_class = data['sclass']
     student_section = data['ssection']
     create_student_info = RetrieveStudentInfo()
-    return jsonify(create_student_info.update_student_info(student_name, student_age, student_class, student_section))
+    if auth == "a1s2d3f4":
+        return jsonify(create_student_info.update_student_info(student_name, student_age, student_class, student_section))
+    else:
+        return jsonify({"message": "Unauthorised access"}), 403
 
 
 if __name__ == "__main__":
