456fa9f4a0acb76383353fb5beff3bd4488638ac
3vikram <viku.vikram05@gmail.com>
2019-02-10 10:44:18 +0530

    Update SchoolAPI.py

--- SchoolAPI.py
+++ SchoolAPI.py
@@ -1,20 +1,33 @@
-from flask import Flask
+from flask import Flask, make_response
 from flask import jsonify
 from flask import request
 from Teachers import *
 from Students import *
+from RequestValidation import *
 
 app = Flask(__name__)
 
 @app.route('/teacher/teacherId/<int:userid>', methods=['GET'])
 def get_teacher_data(userid):
+    headers = request.headers
+    auth = headers.get("API-Secret")
     teacher_instance = RetrieveTeacherInfo()
-    return jsonify(teacher_instance.get_teacher_info(userid))
+    api_token_validation = RequestValidator()
+    if api_token_validation.api_token_validation(auth):
+        return jsonify(teacher_instance.get_teacher_info(userid)), {'X-Frame'}
+    else:
+        return jsonify({"message": "Unauthorised access"}), 403
 
 @app.route('/student/studentId/<int:studentid>', methods=['GET'])
 def get_student_data(studentid):
+    headers = request.headers
+    auth = headers.get("API-Secret")
     student_instance = RetrieveStudentInfo()
-    return jsonify(student_instance.get_student_info(studentid))
+    api_token_validation = RequestValidator()
+    if api_token_validation.api_token_validation(auth):
+        return jsonify(student_instance.get_student_info(studentid))
+    else:
+        return jsonify({"message": "Unauthorised access"}), 403
 
 @app.route('/student/create', methods=['POST'])
 def create_student():
@@ -26,7 +39,8 @@ def create_student():
     student_class = data['sclass']
     student_section = data['ssection']
     create_student_info = RetrieveStudentInfo()
-    if auth == "a1s2d3f4":
+    api_token_validation = RequestValidator()
+    if api_token_validation.api_token_validation(auth):
         return jsonify(create_student_info.set_student_info(student_name, student_age, student_class, student_section))
     else:
         return jsonify({"message": "Unauthorised access"}), 403
@@ -42,7 +56,8 @@ def create_teacher():
     teacher_experience = data['texperience']
     teacher_salary = data['tsalary']
     create_teacher_info = RetrieveTeacherInfo()
-    if auth == "a1s2d3f4":
+    api_token_validation = RequestValidator()
+    if api_token_validation.api_token_validation(auth):
         return jsonify(create_teacher_info.set_teacher_info(teacher_name, teacher_age, teacher_qualification, teacher_experience, teacher_salary))
     else:
         return jsonify({"message": "Unauthorised access"}), 403
@@ -50,7 +65,9 @@ def create_teacher():
 @app.route('/teacher/count', methods=['GET'])
 def total_teachers():
     total_teachers_instance = RetrieveTeacherInfo()
-    return jsonify(total_teachers_instance.get_total_teachers())
+    response= make_response(jsonify(total_teachers_instance.get_total_teachers()))
+    response.headers["X-FRAME-OPTIONS"] = "DENY"
+    return response
 
 @app.route('/student/count', methods=['GET'])
 def total_students():
@@ -64,7 +81,8 @@ def delete_teacher():
     data = request.get_json()
     teacher_ID = data['tid']
     create_teacher_remove = RetrieveTeacherInfo()
-    if auth == "a1s2d3f4":
+    api_token_validation = RequestValidator()
+    if api_token_validation.api_token_validation(auth):
         return jsonify(create_teacher_remove.remove_teacher(teacher_ID))
     else:
         return jsonify({"message": "Unauthorised access"}), 403
@@ -76,7 +94,8 @@ def delete_student():
     data = request.get_json()
     student_ID = data['sid']
     create_student_remove = RetrieveStudentInfo()
-    if auth == "a1s2d3f4":
+    api_token_validation = RequestValidator()
+    if api_token_validation.api_token_validation(auth):
         return jsonify(create_student_remove.remove_student(student_ID))
     else:
         return jsonify({"message": "Unauthorised access"}), 403
@@ -92,7 +111,8 @@ def update_teacher():
     teacher_experience = data['texperience']
     teacher_salary = data['tsalary']
     create_teacher_info = RetrieveTeacherInfo()
-    if auth == "a1s2d3f4":
+    api_token_validation = RequestValidator()
+    if api_token_validation.api_token_validation(auth):
         return jsonify(create_teacher_info.update_teacher_info(teacher_name, teacher_age, teacher_qualification, teacher_experience, teacher_salary))
     else:
         return jsonify({"message": "Unauthorised access"}), 403
@@ -107,7 +127,8 @@ def update_student():
     student_class = data['sclass']
     student_section = data['ssection']
     create_student_info = RetrieveStudentInfo()
-    if auth == "a1s2d3f4":
+    api_token_validation = RequestValidator()
+    if api_token_validation.api_token_validation(auth):
         return jsonify(create_student_info.update_student_info(student_name, student_age, student_class, student_section))
     else:
         return jsonify({"message": "Unauthorised access"}), 403
